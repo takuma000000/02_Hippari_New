503e988e61e96fc9b40f46ae51c978ff8d5d29d1
takuma <k023g0015@g.neec.ac.jp>
2024-06-10 11:31:30 +0900

    02_07 完了

---
+++ Assets/ItemScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Enter");
+        DestroySelf();
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Stay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+    }
+
+}
--- Assets/PullingJump.cs
+++ Assets/PullingJump.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PullingJump : MonoBehaviour
@@ -9,12 +10,13 @@ public class PullingJump : MonoBehaviour
     private Vector3 clickPosition;
     [SerializeField]
     private float jumpPower = 10;
+    private bool isCanJump;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        rb= gameObject.GetComponent<Rigidbody>();
+
+        rb = gameObject.GetComponent<Rigidbody>();
 
     }
 
@@ -29,14 +31,45 @@ public class PullingJump : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            clickPosition=Input.mousePosition;
+            clickPosition = Input.mousePosition;
         }
-        if (Input.GetMouseButtonUp(0))
+        if (isCanJump && Input.GetMouseButtonUp(0))
         {
-            Vector3 dist=clickPosition-Input.mousePosition;
+            Vector3 dist = clickPosition - Input.mousePosition;
             if (dist.sqrMagnitude == 0) { return; }
             rb.velocity = dist.normalized * jumpPower;
         }
 
     }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("è’ìÀÇµÇΩ");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //Debug.Log("ê⁄êGíÜ");
+       // isCanJump = true;
+
+        ContactPoint[] contacts = collision.contacts;
+        Vector3 otherNormal = contacts[0].normal;
+        Vector3 upVector = new Vector3(0, 1, 0);
+        float dotUN=Vector3.Dot(upVector, otherNormal);
+        float dotDeg = Mathf.Acos(dotUN) * Mathf.Rad2Deg;
+        if (dotDeg <= 45)
+        {
+            isCanJump = true;
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //Debug.Log("ó£íEÇµÇΩ");
+        isCanJump = false;
+    }
+
+
 }
